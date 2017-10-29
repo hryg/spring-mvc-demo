fe2ae3b76a3ed57429a04e0e629140f6787f7a88
Henry Heng <hrygheng@gmail.com>
2017-10-29 22:55:00 +0800

    add UserRepository

--- src/main/java/com/hryg/controller/MainController.java
+++ src/main/java/com/hryg/controller/MainController.java
@@ -1,15 +1,31 @@
 package com.hryg.controller;
 
+import com.hryg.model.UserEntity;
+import com.hryg.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
 
 @Controller
 public class MainController {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index() {
         return "index";
+    }
+
+    @RequestMapping(value = "/admin/users", method = RequestMethod.GET)
+    public String getUsers(ModelMap modelMap) {
+        List<UserEntity> userList = userRepository.findAll();
+        modelMap.addAttribute("userList", userList);
+        return "admin/users";
     }
 
 }
